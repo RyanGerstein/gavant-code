4da42eae5415c3ae0856bccdf7597350f9d98443
Ryan Gerstein <ryan.gerstein@gmail.com>
2019-12-18 13:28:03 -0500

    Added in filter functionality
    Filter added to customer page. Checks if project, company, or name loosely match the values. Executes in real time

--- app/controllers/customers/index.js
+++ app/controllers/customers/index.js
@@ -5,17 +5,38 @@ import { sort } from '@ember/object/computed';
 export default Controller.extend({
   sortProperty: 'name',
   sortOptions: null,
+  // filter fields are the fields the filter will check.
+  filterFields: null,
+  filterProperty: null,
 
   init: function() {
     this.sortOptions = {
       'name': ['lastName', 'firstName'],
       'budget': ['budget:desc']
     };
+    this.filterFields = ['lastName', 'firstName'];
   },
 
-  customersSortProps: computed('sortProperty', function() {
+  customersSortProps: computed('sortOptions', 'sortProperty', function() {
     return this.sortOptions[this.sortProperty];
   }),
 
-  sortedCustomers: sort('model', 'customersSortProps')
+  filteredModel: computed('model', 'filterFields', 'filterProperty', function () {
+    if (this.filterProperty && this.filterFields)
+    {
+      let self = this;
+      return this.model.filter(function(item) { 
+        for( let i = 0; i < self.filterFields.length; i++)
+        {
+          if(item[self.filterFields[i]] && item[self.filterFields[i]].toLowerCase().includes(self.filterProperty.toLowerCase())) {
+            return true;
+          }
+        }
+        return false;
+      });
+    }
+    return this.model;
+  }),
+
+  sortedCustomers: sort('filteredModel', 'customersSortProps')
 });
