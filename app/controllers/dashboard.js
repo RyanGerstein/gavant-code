aee1b35cbb519572a9a43b6506967527d0762e7a
Ryan Gerstein <ryan.gerstein@gmail.com>
2019-12-18 19:13:58 -0500

    Linter fixes in dashboard

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -27,12 +27,16 @@ export default Controller.extend({
 
   retiredAccounts: filter('budgetSortCustomers', function(item) { return item.budget == 0 || item.project.toLowerCase() == "retired"; }),
 
-  companyAccountData: computed('model.@each.budget', 'model.@each.company', function() {
+  companyAccountData: computed('model.@each.{budget,company}', function() {
     let data = {};
     let companies = [];
     this.model.forEach(function(item){
       if(!data[item.company]) {
-        data[item.company] = {name: item.company, employees: 0, totalBudget: 0};
+        data[item.company] = {
+          name: item.company,
+          employees: 0,
+          totalBudget: 0
+        };
       }
       data[item.company].employees++;
       data[item.company].totalBudget += item.budget;
